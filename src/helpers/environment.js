31dbb4de278659d107ab3778a13beaa48e6a905b
ikola <ikolajm@gmail.com>
2019-02-22 09:31:32 -0500

    error handle attempt

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,15 +1,11 @@
 let APIURL = '';
 
-switch (window.location.hostname) {
-    // this is the local host name of your react app
-    case 'localhost' || '127.0.0.1':
-        // this is the local host name of your API
+switch( window.location.hostname) {
+    case 'localhost' || '127.0.0.1': 
         APIURL = 'http://localhost:3001';
         break;
-    // this is the deployed react application
-    case 'coop-catalog-jmi.herokuapp.com':
-        // this is the full url of your deployed API
-        APIURL = 'https://coop-catalog-jmi.herokuapp.com'
+    case 'coop-catalog-jmi.herokuapp.com' :
+        APIURL = 'https://coop-catalog-jmi.herokuapp.com';
 }
 
 export default APIURL;
